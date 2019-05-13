10fcfe6dbd9fb25f99c2ad5d4e6b9d696394d128
Marie <pashkova@phystech.edu>
2019-05-13 22:30:23 +0300

    fix bug

--- bot_handlers.py
+++ bot_handlers.py
@@ -47,10 +47,9 @@ def search_film(message):
         print("No response")
         keyboard = types.InlineKeyboardMarkup()
         keyboard.add(types.InlineKeyboardButton(text="Мне повезёт!", callback_data="google"))
-
         
         bot.reply_to(message, "В моих онлайн-сервисах нет этого фильма :(\n\
-            Могу попробовать загуглить его для вас.")
+        Могу попробовать загуглить его для вас.", reply_markup=keyboard)
 
 
 @bot.callback_query_handler(func=lambda call: call.data == "google")
