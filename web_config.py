003865d86c2a671a192054efd92f535a46d31bcd
Marie <pashkova@phystech.edu>
2019-05-13 18:02:18 +0300

    yet another proxy

--- web_config.py
+++ web_config.py
@@ -1,2 +1,2 @@
-proxy_dict = {'https':'socks5://212.22.92.120:56342' }
+proxy_dict = {'https':'socks5://213.251.255.170:9999'}
 header = {"user-agent": 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:66.0) Gecko/20100101 Firefox/66.0'}
