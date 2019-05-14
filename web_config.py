5d747557cfdbe0b0b03e38feb6e4fe69f3d69045
Marie <pashkova@phystech.edu>
2019-05-14 14:21:31 +0300

    THIS is norm proxy

--- web_config.py
+++ web_config.py
@@ -1,2 +1,2 @@
-proxy_dict = {'https':'socks5://213.251.255.170:9999'}
+proxy_dict = {'https':'socks5h://QkgWKA:fte9Gd@213.166.73.194:9637'}
 header = {"user-agent": 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:66.0) Gecko/20100101 Firefox/66.0'}
